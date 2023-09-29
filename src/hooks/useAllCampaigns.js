665333c891d4cc59f2938cec3680c791df60f451
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2023-09-29 09:57:51 +0100

    auto-closed modal and fetched contributors

--- src/hooks/useAllCampaigns.js
+++ src/hooks/useAllCampaigns.js
@@ -2,63 +2,63 @@ import { useEffect, useState } from "react";
 import useCampaignCount from "./useCampaignCount";
 import { useConnection } from "../context/connection";
 import {
-    getCrowdfundContract,
-    getCrowdfundContractWithProvider,
+  getCrowdfundContract,
+  getCrowdfundContractWithProvider,
 } from "../utils";
 
 const useAllCampaigns = () => {
-    const [campaigns, setCampaigns] = useState([]);
-    const { provider } = useConnection();
-    const campaignNo = useCampaignCount();
+  const [campaigns, setCampaigns] = useState([]);
+  const { provider } = useConnection();
+  const campaignNo = useCampaignCount();
 
-    useEffect(() => {
-        const fetchAllCampaigns = async () => {
-            try {
-                const contract = await getCrowdfundContract(provider, false);
-                const campaignsKeys = Array.from(
-                    { length: Number(campaignNo) },
-                    (_, i) => i + 1
-                );
-                const campaignPromises = campaignsKeys.map((id) =>
-                    contract.crowd(id)
-                );
+  useEffect(() => {
+    const fetchAllCampaigns = async () => {
+      try {
+        const contract = await getCrowdfundContract(provider, false);
+        const campaignsKeys = Array.from(
+          { length: Number(campaignNo) },
+          (_, i) => i + 1
+        );
+        const campaignPromises = campaignsKeys.map((id) => contract.crowd(id));
+        const contributorPromises = campaignsKeys.map((id) =>
+          contract.getContributors(id)
+        );
 
-                const campaignResults = await Promise.all(campaignPromises);
+        const campaignResults = await Promise.all(campaignPromises);
+        const contributorResults = await Promise.all(contributorPromises);
 
-                const campaignDetails = campaignResults.map(
-                    (details, index) => ({
-                        id: campaignsKeys[index],
-                        title: details.title,
-                        fundingGoal: details.fundingGoal,
-                        owner: details.owner,
-                        durationTime: Number(details.durationTime),
-                        isActive: details.isActive,
-                        fundingBalance: details.fundingBalance,
-                        contributors: details.contributors,
-                    })
-                );
+        const campaignDetails = campaignResults.map((details, index) => ({
+          id: campaignsKeys[index],
+          title: details.title,
+          fundingGoal: details.fundingGoal,
+          owner: details.owner,
+          durationTime: Number(details.durationTime),
+          isActive: details.isActive,
+          fundingBalance: details.fundingBalance,
+          contributors: contributorResults[index],
+        }));
+        setCampaigns(campaignDetails);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
 
-                setCampaigns(campaignDetails);
-            } catch (error) {
-                console.error("Error fetching campaigns:", error);
-            }
-        };
+    fetchAllCampaigns();
+    console.log(campaigns);
 
-        fetchAllCampaigns();
+    // Listen for event
+    const handleProposeCampaignEvent = (id, title, amount, duration) => {
+      console.log({ id, title, amount, duration });
+    };
+    const contract = getCrowdfundContractWithProvider(provider);
+    contract.on("ProposeCampaign", handleProposeCampaignEvent);
 
-        // Listen for event
-        const handleProposeCampaignEvent = (id, title, amount, duration) => {
-            console.log({ id, title, amount, duration });
-        };
-        const contract = getCrowdfundContractWithProvider(provider);
-        contract.on("ProposeCampaign", handleProposeCampaignEvent);
+    return () => {
+      contract.off("ProposeCampaign", handleProposeCampaignEvent);
+    };
+  }, [campaignNo, provider]);
 
-        return () => {
-            contract.off("ProposeCampaign", handleProposeCampaignEvent);
-        };
-    }, [campaignNo, provider]);
-
-    return campaigns;
+  return campaigns;
 };
 
 export default useAllCampaigns;
--- src/hooks/useContribute.js
+++ src/hooks/useContribute.js
@@ -4,33 +4,32 @@ import { calculateGasMargin, getCrowdfundContract } from "../utils";
 import useCampaignCount from "./useCampaignCount";
 
 const useContribute = () => {
-    const { provider, isActive } = useConnection();
-    const campaignLength = useCampaignCount();
-    const contribute = useCallback(
-        async (id, amount) => {
-            console.log({ amount });
-            if (!isActive || !provider) return;
-            if (!campaignLength) return;
-            if (Number(id) > campaignLength)
-                return alert("campaign does not exist");
-            const contract = await getCrowdfundContract(provider, true);
+  const { provider, isActive } = useConnection();
+  const campaignLength = useCampaignCount();
+  const contribute = useCallback(
+    async (id, amount) => {
+      console.log({ amount });
+      if (!isActive || !provider) return;
+      if (!campaignLength) return;
+      if (Number(id) > campaignLength) return alert("campaign does not exist");
+      const contract = await getCrowdfundContract(provider, true);
 
-            const estimatedGas = await contract.contributeEth.estimateGas(
-                Number(id),
-                {
-                    value: amount,
-                }
-            );
+      const estimatedGas = await contract.contributeEth.estimateGas(
+        Number(id),
+        {
+          value: amount,
+        }
+      );
 
-            return contract.contributeEth(Number(id), {
-                value: amount,
-                gasLimit: calculateGasMargin(estimatedGas),
-            });
-        },
-        [campaignLength, isActive, provider]
-    );
+      return contract.contributeEth(Number(id), {
+        value: amount,
+        gasLimit: calculateGasMargin(estimatedGas),
+      });
+    },
+    [campaignLength, isActive, provider]
+  );
 
-    return contribute;
+  return contribute;
 };
 
 export default useContribute;
